e91db82d07a802b81b12603d0cbc71c1d370d905
tahirikosan <tahirabuzetoglu@gmail.com>
2020-04-25 00:37:39 +0300

    Register sayfası okey

--- src/java/DBLayerTahir.java
+++ src/java/DBLayerTahir.java
@@ -22,12 +22,17 @@ import java.sql.Statement;
 
 
 
-public class DBLayer {
+public class DBLayerTahir {
     
     //column names
-    private int ID = 1;
-    private int NAME = 2;
+    private int NAME = 1;
+    private int SURNAME = 2;
     private int PASSWORD = 3;
+    private int EMAIL = 4;
+    private int GENDER = 5;
+    private int HEIGHT = 6;
+    private int WEIGHT = 7;
+    private int AGE = 8; 
     
     
     private Connection conn;
@@ -62,7 +67,7 @@ public class DBLayer {
             ResultSet result = statement.executeQuery("SELECT * FROM kullanici");
             
             while(result.next()){
-              return result.getString(ID) + " " + result.getString(NAME) + " " + result.getString(PASSWORD);
+              return result.getString(EMAIL) + " " + result.getString(NAME) + " " + result.getString(PASSWORD);
             }
             
         }catch(SQLException e){
@@ -81,15 +86,23 @@ public class DBLayer {
         
         try{
            
-            String query = "INSERT INTO kullanici VALUES (?,?,?,?,?,?,?,?)";
+            String query = "INSERT INTO kullanici (name, surname, password, email, gender, height, weight, age) VALUES (?,?,?,?,?,?,?,?)";
             
             PreparedStatement statement = conn.prepareStatement(query);
-            //statement.setString(1, Na);
-    
-            int result = statement.executeUpdate();
+            statement.setString(NAME, user.getName());
+            statement.setString(SURNAME, user.getSurname());
+            statement.setString(PASSWORD, user.getPassword());
+            statement.setString(EMAIL, user.getEmail());
+            statement.setString(GENDER, user.getGender());
+            statement.setInt(HEIGHT, user.getHeight());
+            statement.setInt(WEIGHT, user.getWeight());
+            statement.setInt(AGE, user.getAge());
            
-       
-            if(result != 0){
+           
+            int result = statement.executeUpdate();
+          
+            
+            if(result == 1){
                 return true;
             }else{
                 return false;
@@ -103,11 +116,11 @@ public class DBLayer {
     }
     
     public static void main(String[] args) {
-        DBLayer db = new DBLayer();
+        DBLayerTahir db = new DBLayerTahir();
         
         db.connect();
         
-        User user = new User(1, "mokko",  "mokko",  "mokko",  "mokko", 0,  "mokko", 0, 0);
+        User user = new User(1, "defter",  "abc",  "mokko",  "mokko", 0,  "mokko", 0, 0);
         db.registerUser(user);
         
         
